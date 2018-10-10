f35a534362c7289736c33f6898dd6b69896eb570
elraro <elraro@elraro.eu>
2018-10-10 18:55:28 +0000

    fixed bucket url

--- app/bot.py
+++ app/bot.py
@@ -30,7 +30,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbosity", help="Defines log verbosity",
                     choices=['CRITICAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'], default='INFO')
 parser.add_argument("-b", "--bucket", help="Bucket or url where audios are stored",
-                    default='http://rajoy.elraro.eu/')
+                    default='https://github.com/elraro/RajoyBot/raw/master/RajoyBotSounds/')
 parser.add_argument("--sqlite", help="SQLite file path")
 parser.add_argument("--mysql-host", help="mysql host")
 parser.add_argument("--mysql-port", type=str, help="mysql port", default='3306')
