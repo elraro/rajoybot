908d24d8b955495e25456f3194f5e3fd505a1f97
Alberto <elraro@elraro.eu>
2022-04-29 00:11:49 +0200

    Update webhook.py

--- app/webhook.py
+++ app/webhook.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from utils.loggerfactory import LoggerFactory
+from persistence.loggerfactory import LoggerFactory
 from aiohttp import web
 import telebot
 
